83b350428ffa1e98ceb423b74e0a307ded7c2a87
Lucidus <10741504+lucidus@user.noreply.gitee.com>
2023-04-15 23:56:36 +0800

    登录优化

--- src/pages/MyHome.vue
+++ src/pages/MyHome.vue
@@ -13,14 +13,35 @@
                         <el-tab-pane label="个人中心" name="fourth"></el-tab-pane>
                     </el-tabs>
                 </div>
+                <div class="userLogin">
+                    <div v-if="!userNameShow">
+                        <el-button round @click="dialogFormVisible = true">登录</el-button>
+                        <el-button round>注册</el-button>
+                    </div>
+                    <div class="userLogin_flex" v-else>
+                        <el-avatar size="small" :src="circleUrl"></el-avatar>
+                        <span>{{ userName }}</span>
+                    </div>
+                    <el-dialog title="登录" :visible.sync="dialogFormVisible">
+                        <el-form class="login-form" :rules="rules" ref="formLogin" :label-position="'right'"
+                            label-width="70px" :model="formLogin">
+                            <el-form-item label="账号：" prop="username" required>
+                                <el-input v-model="formLogin.username"></el-input>
+                            </el-form-item>
+                            <el-form-item label="密码：" prop="password" required>
+                                <el-input type="password" v-model="formLogin.password"></el-input>
+                            </el-form-item>
+                            <el-form-item style="margin-left: 0 !important;">
+                                <el-button type="primary" @click="submitLogin('formLogin')">登录</el-button>
+                                <el-button @click="dialogFormVisible = false">取消</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </el-dialog>
+                </div>
                 <div class="search">
                     <el-input placeholder="请输入内容" prefix-icon="el-icon-search" v-model="searchContent">
                     </el-input>
                 </div>
-                <div class="userLogin">
-                    <router-link to="login">登录</router-link>
-                </div>
-
             </el-header>
             <el-main>Main</el-main>
             <el-footer>Footer</el-footer>
@@ -35,12 +56,63 @@ export default {
         return {
             activeName: 'second',//当前标签
             searchContent: '',//当前搜索内容
+            circleUrl: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
+            userNameShow: false,//用户登录名显示
+            userName: '',
+            dialogFormVisible: false,//登录框的显示
+            formLogin: {
+                username: 'forzhang',//登录账号
+                password: 'xxxxxxxx'//登录密码
+            },
+            rules: {//校验规则
+                username: [
+                    { required: true, message: '请输入账号名称', trigger: 'blur' },
+                    { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入密码' },
+                    { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+                ]
+
+            }
         };
     },
     methods: {
         // 点击标签切换
         handleClick(tab, event) {
             console.log(tab, event);
+        },
+        // 登录验证
+        submitLogin(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    // 发送 POST 请求
+                    this.axios({
+                        method: 'post',
+                        url: 'account/sign_in/',
+                        data: {
+                            username: this.formLogin.username,
+                            password: this.formLogin.password
+                        }
+                    }).then((response) => {
+                        this.$message({
+                            message: response.msg,
+                            type: 'success'
+                        });
+                        // 登录成功显示用户名
+                        this.userNameShow = true;
+                        localStorage.setItem('username', response.data.username);
+                        this.userName = localStorage.getItem('username');
+                        // 登录成功登录框隐藏
+                        this.dialogFormVisible = false;
+                    }).catch((error) => {
+                        console.log(error);
+                    });
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
         }
     }
 }
@@ -63,13 +135,23 @@ export default {
     }
 
     .search {
-        float: left;
+        float: right;
         width: 280px;
+        margin-right: 10px;
     }
 
     .userLogin {
         float: right;
-        width: 76px;
+        width: 162px;
+        height: 60px;
+
+        .userLogin_flex {
+            float: right;
+            width: 108px;
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+        }
     }
 }
 
@@ -110,5 +192,9 @@ body>.el-container {
 
 ::v-deep .el-tabs__item:hover {
     color: #e2f46f;
+}
+
+::v-deep .el-input__inner {
+    height: 26px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
 import VueRouter from 'vue-router'
-import MyLogin from '../pages/MyLogin'
 import MyHome from '../pages/MyHome'
 export default new VueRouter({
     routes: [
         {
             path: '/',
             redirect: '/home',
-        },
-        {
-            path: '/login',
-            component: MyLogin
         },
         {
             path: '/home',
