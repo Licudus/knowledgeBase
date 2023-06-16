ef3df9fc1e20444a93e18d673669400392af09b7
zhangwei <2334948340@qq.com>
2023-06-16 15:30:01 +0800

    文章详情页

--- src/pages/MyArticleDetails.vue
+++ src/pages/MyArticleDetails.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="container">
-    <el-card class="box-card">edf</el-card>
+    <el-card class="box-card">
+      <h1>爱你就像爱生命</h1>
+      <div class="iconAll">
+        <div>
+          <el-button type="success" round>成功按钮</el-button>
+        </div>
+        <div class="iconBottom">
+          <img src="../assets/person.png" alt />
+          <span>哈比老师</span>
+        </div>
+        <div class="iconBottom">
+          <img src="../assets/person.png" alt />
+          <span>9999-99-99</span>
+        </div>
+        <div class="iconBottom">
+          <img src="../assets/person.png" alt />
+          <span>轻松</span>
+        </div>
+      </div>
+    </el-card>
     <!-- 个人中心 -->
     <MyPersonal></MyPersonal>
     <!-- 热门文章 -->
@@ -21,8 +40,20 @@ export default {
 .container {
   position: relative;
   height: 100%;
-  width: 60%;
+  width: 70%;
   color: #ccc;
   margin: 60px auto;
+  .iconAll {
+    display: flex;
+
+    .iconBottom {
+      margin-right: 10px;
+
+      img {
+        width: 20px;
+        margin-right: 6px;
+      }
+    }
+  }
 }
 </style>
